d3314f227d92fdde7d00074d4884a507d8e3cde7
Salimou Diaby <salimoudiaby3028@gmail.com>
2026-04-18 00:10:28 +0000

    Fix CORS - send single origin header

--- backend/server.js
+++ backend/server.js
@@ -163,15 +163,20 @@ const allowedOrigins = (process.env.CORS_ORIGIN || 'http://localhost:5173')
 
 app.use(cors({
   origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, origin); // ← renvoie uniquement l'origine correspondante
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
+
+// Gérer les requêtes OPTIONS (preflight)
+app.options('*', cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
